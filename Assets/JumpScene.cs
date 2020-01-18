53871d606345e83501df31ef3ed9589c7071175b
halozxc <824927872@qq.com>
2020-01-18 23:26:26 +0800

    Sample

---
+++ Assets/JumpScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+public string nextScene;
+
+private void SceneJump(){
+ SceneManager.LoadSceneAsync(nextScene);
+}
+
+private void OnCollisionEnter(Collision other)
+{
+    
+}
+}
